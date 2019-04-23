f057e33ec58ba666cbcfdb178038541e1ee17370
Aleksandar Milicevic <almili@microsoft.com>
2019-04-23 16:27:28 -0700

    wip

--- Public/Src/Utilities/Native/IO/Unix/FileSystem.Unix.cs
+++ Public/Src/Utilities/Native/IO/Unix/FileSystem.Unix.cs
@@ -550,8 +550,6 @@ namespace BuildXL.Native.IO.Unix
         {
             try
             {
-                var mayBeExistence = TryProbePathExistence(path, openSymlink);
-
                 handle = Open(path, CreateOpenFlags(desiredAccess, shareMode, fileMode, openSymlink), CreateFilePermissions(desiredAccess));
 
                 if (handle.IsInvalid)
@@ -560,14 +558,15 @@ namespace BuildXL.Native.IO.Unix
                     return CreateErrorResult(Marshal.GetLastWin32Error());
                 }
 
-                int successCode =
-                    mayBeExistence.Succeeded && mayBeExistence.Result == PathExistence.ExistsAsFile
-                    ? NativeIOConstants.ErrorFileExists
-                    : (mayBeExistence.Succeeded && mayBeExistence.Result == PathExistence.ExistsAsDirectory
-                        ? NativeIOConstants.ErrorAlreadyExists
-                        : NativeIOConstants.ErrorSuccess);
+                //var mayBeExistence = TryProbePathExistence(path, openSymlink);
+                //int successCode =
+                //    mayBeExistence.Succeeded && mayBeExistence.Result == PathExistence.ExistsAsFile
+                //    ? NativeIOConstants.ErrorFileExists
+                //    : (mayBeExistence.Succeeded && mayBeExistence.Result == PathExistence.ExistsAsDirectory
+                //        ? NativeIOConstants.ErrorAlreadyExists
+                //        : NativeIOConstants.ErrorSuccess);
 
-                return OpenFileResult.Create(path, successCode, fileMode, handleIsValid: true);
+                return OpenFileResult.Create(path, NativeIOConstants.ErrorSuccess, fileMode, handleIsValid: true);
             }
             catch (Exception e)
             {
@@ -873,9 +872,14 @@ namespace BuildXL.Native.IO.Unix
         /// <inheritdoc />
         public Possible<string> TryGetReparsePointTarget(SafeFileHandle handle, string sourcePath)
         {
+            return TryGetReparsePointTarget(sourcePath);
+        }
+
+        internal Possible<string> TryGetReparsePointTarget(string sourcePath)
+        {
             try
             {
-                var maxPathLength = BuildXL.Native.IO.NativeIOConstants.MaxPath + 1;
+                var maxPathLength = NativeIOConstants.MaxPath + 1;
                 var sb = new StringBuilder(maxPathLength);
                 long numCharactersWritten = SafeReadLink(sourcePath, sb, maxPathLength);
                 if (numCharactersWritten >= 0 && numCharactersWritten <= maxPathLength)
