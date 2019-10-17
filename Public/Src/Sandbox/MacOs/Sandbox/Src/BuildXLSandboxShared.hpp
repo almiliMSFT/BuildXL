9c90946f7b52140eee1afbdf6a4e9b2e37bdbfd7
Aleksandar Milicevic <almili@microsoft.com>
2019-10-17 14:18:38 -0700

    3

--- Public/Src/Sandbox/MacOs/Sandbox/Src/BuildXLSandboxShared.hpp
+++ Public/Src/Sandbox/MacOs/Sandbox/Src/BuildXLSandboxShared.hpp
@@ -286,11 +286,7 @@ extern os_log_t logger;
 #define log(format, ...) os_log(logger, "[[ %s ]] %s: " #format "\n", kBuildXLSandboxClassName, __func__, __VA_ARGS__)
 #define log_error(format, ...) os_log_error(logger, "[[ %s ]][ERROR] %s: " #format "\n", kBuildXLSandboxClassName, __func__, __VA_ARGS__)
 
-#if DEBUG
 #define log_debug(format, ...) log(format, __VA_ARGS__)
-#else
-#define log_debug(format, ...)
-#endif
 
 #define log_verbose(isEnabled, format, ...) if (isEnabled) log(format, __VA_ARGS__)
 
