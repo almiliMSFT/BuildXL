42f6d5fa10ff14b14d38b5b0d91fe3679785d2fe
Aleksandar Milicevic <almili@microsoft.com>
2019-10-17 14:25:47 -0700

    4

--- Public/Src/Sandbox/MacOs/Sandbox/Src/SysCtl.cpp
+++ Public/Src/Sandbox/MacOs/Sandbox/Src/SysCtl.cpp
@@ -3,13 +3,8 @@
 
 #include "SysCtl.hpp"
 
-#if DEBUG
 int g_bxl_enable_counters = 1;
 int g_bxl_verbose_logging = 1;
-#else
-int g_bxl_enable_counters = 0;
-int g_bxl_verbose_logging = 0;
-#endif
 
 SYSCTL_INT(_kern,                               // parent
            OID_AUTO,                            // oid
